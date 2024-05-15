76fbf9e05e256fb998e610386042b93c54738435
Ronald Abu Saleh <ronald.abusaleh@gmail.com>
2024-05-15 16:27:14 -0400

    add extension endpoint

--- src/index.js
+++ src/index.js
@@ -192,6 +192,31 @@ app.get('/api/subscription/:id', async (req, res) => {
 });
 
 /**
+ * Get extensions
+ */
+app.get('/api/extension', async (req, res) => {
+  try {
+    const rcsdk = new RC({
+      server: process.env.RC_SERVER_URL,
+      clientId: process.env.RC_CLIENT_ID,
+      clientSecret: process.env.RC_CLIENT_SECRET,
+    });
+
+    const platform = rcsdk.platform();
+
+    await platform.login({ jwt: process.env.RC_JWT });
+
+    const request = await platform.get(`/restapi/v1.0/account/2984913036/extension`);
+    const response = await request.json();
+
+    res.json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+/**
  * Create subscription
  */
 app.post('/api/subscription', async (req, res) => {
